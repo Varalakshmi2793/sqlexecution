21742ce5c8bfb291b48ca46973bf2bc51454d733
Varalakshmmi2793 <varaluckky.2@gmail.com>
2024-04-17 11:02:22 +0530

    association

--- 00-starting-setup/util/database.js
+++
@@ -1,6 +0,0 @@
-const sequelize= require('sequelize');
- const seq= new sequelize('node-complete', 'root', 'Password@123',{
-    dialect:' mysql',
-    host: 'localhost'
- });
- module.exports=sequelize;
